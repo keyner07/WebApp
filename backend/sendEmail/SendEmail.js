28eff25b536638f102ebadc1624cd9ced4ad2075
keyner07 <skerling19@hotmail.com>
2019-11-01 14:25:00 -0400

    Send email

---
+++ backend/sendEmail/SendEmail.js
@@ -0,0 +1,38 @@
+const nodemailer = require('nodemailer');
+var transporter = nodemailer.createTransport({
+    host: 'smtp-mail.outlook.com',
+    secureConnection: false, // TLS requires secureConnection to be false
+    port: 587, // port for secure SMTP
+    tls: {
+       ciphers:'SSLv3'
+    },
+    auth: {
+        user: 'skerling19@hotmail.com',
+        pass: 'michelle07'
+    }
+});
+function sendMail(ToEmail){
+    return new Promise((resolve, reject) => {
+        var mailOption = {
+            from: 'skerling19@hotmail.com',
+            to: ToEmail,
+            subject: 'Te toca llamar y pedir la orden',
+            text: `Buenas, tienes que llamar a la heladeria y pedir la orden.
+                    Heladeria Rantic. Tel:809-876-1234`
+        };
+        transporter.sendMail(mailOption, function(error, info){
+                if(error){
+                    console.error(error);
+                    reject();
+                }
+                else{
+                    console.log(`Email sent: ${info.response}`);
+                    resolve();
+                }
+        })
+    
+    })
+}
+module.exports = {
+    sendMail
+}
